37f0a54073b6dafddb4faebcd0cce15046457fe6
Luis Ciber <32603705+luiscib3r@users.noreply.github.com>
2023-12-13 13:08:08 -0300

    chore(api-v3): Update main.ts

--- apps/api-v3/src/main.ts
+++ apps/api-v3/src/main.ts
@@ -10,10 +10,9 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'api/v3';
   app.setGlobalPrefix(globalPrefix);
-  // const port = process.env.PORT || 3000;
-  const port = 3002;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
